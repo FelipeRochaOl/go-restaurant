c2a02e7e1ab301681256692b034718be94cb5d51
Felipe Rocha <feliperochaoliveira@gmail.com>
2022-10-27 13:57:18 -0300

    Update production url axios service

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://localhost:3333/.netlify/functions'
+    ? 'https://go-restaurant.felipe-rocha.com/.netlify/functions'
     : 'http://localhost:3333';
 
 const api = axios.create({
